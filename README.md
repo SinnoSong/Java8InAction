a53fc3edf104bdd5f1dff9b081405eee9821bac3
sinno.song <sinno.song@outlook.com>
2023-01-07 15:54:33 +0800

    init repo

---
+++ README.md
@@ -0,0 +1,2 @@
+# java8 in action 
+This repo is study *java8 in action* sample codes.
---
+++ lambdasinaction/src/test/java/manning/AppTest.java
@@ -0,0 +1,20 @@
+package manning;
+
+import static org.junit.Assert.assertTrue;
+
+import org.junit.Test;
+
+/**
+ * Unit test for simple App.
+ */
+public class AppTest 
+{
+    /**
+     * Rigorous Test :-)
+     */
+    @Test
+    public void shouldAnswerWithTrue()
+    {
+        assertTrue( true );
+    }
+}
